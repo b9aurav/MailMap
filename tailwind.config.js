7de8b4d98ece8b3bf162cc1d25811ead28a18658
Gaurav <hey9aurav@gmail.com>
2024-02-17 12:24:58 +0530

    Skeleton UI integrated.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
+import { skeleton } from '@skeletonlabs/tw-plugin'
+import { join } from 'path'
 /** @type {import('tailwindcss').Config} */
+
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    skeleton({
+      themes: { preset: [ "crimson" ] }
+    })
+  ],
 }
 
